cf0d1c9c28cbe638dababdc8db034f5183be4dea
johyemtol <144199666+johyemtol@users.noreply.github.com>
2023-09-13 14:50:36 +0900

    Update arithmeticShort.cpp

--- practice/week2/arithmeticShort.cpp
+++ practice/week2/arithmeticShort.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
-	int two = 2;
+	//int 값 넣기
+	int two = 2;	
 	int eight = 8;
 	int sum1 = 2;
 	int sum2 = 2;
+	//sum 1,2값을 사칙연산을 이용해서 넣기
 	sum1 = sum1 + two;
 	sum2 += eight;
 	cout << "sum1 = " << sum1 << endl;
