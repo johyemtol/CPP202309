3dfc6fbf2659f90a654d4538a1f887113cd0811d
johyemtol <144199666+johyemtol@users.noreply.github.com>
2023-09-13 14:51:59 +0900

    Update cout2.cpp

--- practice/week2/cout2.cpp
+++ practice/week2/cout2.cpp
@@ -9,6 +9,7 @@ int main() {
 	bool is_true = true;
 	string word = "Hello World";
 
+	//cout 통해 위에 저장해둔 값들 출력하기
 	cout << "char : " << character << endl;
 	cout << "int : " << integer << endl;
 	cout << "double : " << precision << endl;
