e32d452fa22c03c9c31245605a7a8468c510d8e9
hyemtol <jhmlh0308@naver.com>
2023-12-01 13:09:34 +0900

    week 13. mud_game_extend

---
+++ challenge/week13/mud_game_extend.cpp/user.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class User
+{
+private:
+	int hp;
+public:
+	User() :hp(20) {}
+	int GetHp();
+	void DecreaseHp(int dec_hp);
+	void IncreaseHp(int inc_hp);
+	virtual void doAttack();
+};
+
+class Magician :public User {
+public:
+	void doAttack() override;
+};
+
+class Warrior :public User {
+public:
+	void doAttack() override;
+};
